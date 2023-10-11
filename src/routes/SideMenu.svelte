d68f899cf3586a51c6046dc8470063150ffa7924
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-10-11 16:58:14 +0100

    Add: LogOut Fuctionality

--- src/routes/SideMenu.svelte
+++ src/routes/SideMenu.svelte
@@ -1,13 +1,13 @@
 <script>
 
   import { page } from '$app/stores'
+  import { authHandlers } from '../store/store';
 
   $: currentRoute = $page.route.id;
 
 
 
   let menuItems = [
-    { text: 'Home', link: '', icon: 'fa-home' },
     { text: 'Dashboard', link: 'dashboard', icon: 'fa-dashboard' },
     { text: 'Properties', link: 'properties', icon: 'fa-house' },
     { text: 'Inbox', link: 'inbox', icon: 'fa-envelope' },
@@ -30,18 +30,11 @@
   <div class="bottom links">
     <a class="menu-item support {currentRoute == '/support'? 'active': ''}" href='/support'><i class="fa fa-question-circle"></i> Support</a>
     <a class="menu-item settings {currentRoute == '/settings'? 'active': ''}" href='/settings'><i class="fa fa-cog"></i> Settings</a>
-    <a class="menu-item profile {currentRoute == '/profile'? 'active': ''}" href='/profile'><i class="fa fa-user"></i> Profile Name</a>
+    <button class="menu-item profile " on:click={authHandlers.logout}><i class="fa-solid fa-arrow-right-from-bracket"></i> Log out</button>
   </div>
 </nav>
 
 <style>
-
-  .active, .active > .fa {
-    background-color: black;
-    color: white;
-    border-radius: 10px;
-  }
-
 
   .active.support, .active.support > .fa {
     background-color: rgba(0, 80, 36, 0.788);
@@ -103,13 +96,37 @@
     margin: 2em 1em;
   }
 
-  .fa {
+  .fa, .fa-solid {
     text-align: center;
     width: 14px;
     font-size: 16px;
     color: rgba(0, 0, 0, 0.345);
     margin: 0 5px;
 }
+
+button {
+    color: black;
+    text-decoration: none;
+    font-size: 14px;
+    padding: 10px;
+    border-radius: 10px;
+    border: 1px solid rgba(0, 0, 0, 0.20);
+    text-align: left;
+    background-color: transparent;
+    cursor: pointer;
+  }
+
+  button:hover {
+    color: coral;
+    
+    border: 1px solid coral;
+    text-align: left;
+    background-color: transparent;
+  }
+
+  button:hover > .fa-solid {
+    color: rgb(255, 0, 0);
+  }
 
   a {
     color: black;
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { writable } from "svelte/store";
+import { auth } from "../lib/firebase/firebase";
 
 export const authStore = writable({
   user: null,
