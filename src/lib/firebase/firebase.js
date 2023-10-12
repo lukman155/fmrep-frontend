51cb6f2727d33c56402c8881546da73f767bf9f0
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-10-12 16:41:40 +0100

    Update firebase.js

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -7,7 +7,6 @@ import { getAnalytics } from "firebase/analytics";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
   const firebaseConfig = {
     apiKey: process.env.VITE_API_KEY,
