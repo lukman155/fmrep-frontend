6260f3e8e12fd7f7abb10fe89613b448194e21da
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-10-12 16:36:17 +0100

    Update firebase.js

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -11,12 +11,12 @@ import { getAnalytics } from "firebase/analytics";
 
   const firebaseConfig = {
     apiKey: process.env.VITE_API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
+    authDomain: process.env.VITE_AUTH_DOMAIN,
+    projectId: process.env.VITE_PROJECT_ID,
+    storageBucket: process.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
+    appId: process.env.VITE_APP_ID,
+    measurementId: process.env.VITE_MEASUREMENT_ID
   }
   
 
